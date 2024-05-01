a2d01e53247d7400056c9b853b00fd4743999b0c
HirCoir <97632765+HirCoir@users.noreply.github.com>
2024-05-01 05:09:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ model_names = {
         "replacements": [('\n', ' . '), ('*', '')]
     },
     "Español México | Laura Neuronal": {
-        "model_path": "es_MX-laura-high.onnx,
+        "model_path": "es_MX-laura-high.onnx",
         "replacements": [('\n', ' . '), ('*', '')]
     },
     "Español México | Emma Neuronal": {
