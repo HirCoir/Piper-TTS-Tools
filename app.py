6a511b76ceb604b1217cb88d90c8f7b5e9d3e51e
HirCoir <97632765+HirCoir@users.noreply.github.com>
2024-05-14 21:35:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ model_names = {
     }
 }
 
-
 # Variable para almacenar el token secreto
 secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
 
@@ -55,11 +54,26 @@ def multiple_replace(text, replacements):
         text = text.replace(old, new)
     return text
 
-def filter_text(text, model_name):
+def remove_emojis(text):
+    # Definir un patrón regex para los emojis
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "\U00002702-\U000027B0"  # Dingbats
+        "\U000024C2-\U0001F251" 
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+def filter_text(text, model_name, remove_emojis_flag=False):
     if model_name in model_names:
         replacements = model_names[model_name]["replacements"]
         # Realizar reemplazos específicos del modelo
         filtered_text = multiple_replace(text, replacements)
+        if remove_emojis_flag:
+            filtered_text = remove_emojis(filtered_text)
         # Eliminar caracteres especiales usando shlex
         filtered_text = shlex.quote(filtered_text)
         return filtered_text
@@ -68,8 +82,8 @@ def filter_text(text, model_name):
         return None
 
 # Define una función para convertir texto a voz
-def convert_text_to_speech(text, model_name):
-    filtered_text = filter_text(text, model_name)[:50000]
+def convert_text_to_speech(text, model_name, remove_emojis_flag=True):
+    filtered_text = filter_text(text, model_name, remove_emojis_flag)[:50000]
     if filtered_text is None:
         return None
 
@@ -107,17 +121,18 @@ def authenticate_request():
 @app.route('/convert', methods=['POST'])
 def convert_text():
     auth_response = authenticate_request()
-    if auth_response:
+    if (auth_response):
         return auth_response
-    
+
     data = request.get_json()
     if 'text' not in data or 'model' not in data:
         return jsonify({"error": "Missing 'text' or 'model' in request."}), 400
-    
+
     text = data['text']
     model_name = data['model']
-    
-    audio_base64 = convert_text_to_speech(text, model_name)
+    remove_emojis_flag = data.get('remove_emojis', False)
+
+    audio_base64 = convert_text_to_speech(text, model_name, remove_emojis_flag)
     if audio_base64:
         return jsonify({"audio_base64": audio_base64})
     else:
@@ -126,7 +141,7 @@ def convert_text():
 @app.route('/models', methods=['GET'])
 def list_models():
     auth_response = authenticate_request()
-    if auth_response:
+    if (auth_response):
         return auth_response
 
     model_options = list(model_names.keys())
